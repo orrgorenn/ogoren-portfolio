6f13e2e8ac13ab1fded5216a89baa7385e956983
Orr Goren <orrgorenn@gmail.com>
2024-09-23 00:00:06 +0300

    add autoprefixer, update tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,17 +3,26 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1200px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFUALT: "1rem",
+        md: "2rem",
       },
     },
+    extend: {},
   },
   plugins: [],
 };
+
 export default config;
