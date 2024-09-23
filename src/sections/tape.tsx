1d30f2d7690e0e2a2faeef8f589e29235852b386
Orr Goren <orrgorenn@gmail.com>
2024-09-24 00:20:51 +0300

    add format

--- src/sections/tape.tsx
+++ src/sections/tape.tsx
@@ -20,7 +20,7 @@ export default function Tape() {
     <section className="overflow-x-clip py-16 lg:py-24">
       <div className="-mx-1 -rotate-3 bg-gradient-to-r from-emerald-300 to-sky-400">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="animate-move-left flex flex-none gap-4 py-3 pr-4">
+          <div className="flex flex-none animate-move-left gap-4 py-3 pr-4">
             {[...new Array(2)].fill(0).map((_, idx) => (
               <Fragment key={idx}>
                 {words.map((word) => (
